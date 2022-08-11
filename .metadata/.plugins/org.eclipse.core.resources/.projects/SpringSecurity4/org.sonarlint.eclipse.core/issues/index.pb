ec3387090155d72015f94111ffd9b5de4d329f61
vikashkmr519 <vikashkmr519@gmail.com>
2022-08-11 23:14:06 +0530

    filter chain

--- .metadata/.plugins/org.eclipse.core.resources/.projects/SpringSecurity4/org.sonarlint.eclipse.core/issues/index.pb
+++ .metadata/.plugins/org.eclipse.core.resources/.projects/SpringSecurity4/org.sonarlint.eclipse.core/issues/index.pb
@@ -5,3 +5,7 @@ r
 Bsrc/main/java/com/vikash/springSecurity/config/SecurityConfig.java,d/4/d4cb5f3daddd3f06e3e6399d7e23e91dd7830c30
 v
 Fsrc/main/java/com/vikash/springSecurity/config/MyCustAuthProvider.java,5/d/5de9c905fd27ad7d7cbf63136658f5df1b891464
+v
+Fsrc/main/java/com/vikash/springSecurity/controller/HomeController.java,d/2/d214e675500e49aa2c7e5c6a3731d935a46846f5
+k
+;src/main/java/com/vikash/springSecurity/config/Config2.java,2/8/28b7a08da340045e657b7ceb9d36dd15b6ab1dea
--- SpringSecurity5/src/main/java/com/vikash/springSecurity/filter/MyBasicCustomAuthenticationFilter.java
+++ SpringSecurity5/src/main/java/com/vikash/springSecurity/filter/MyBasicCustomAuthenticationFilter.java
@@ -1,5 +1,32 @@
 package com.vikash.springSecurity.filter;
 
-public class MyBasicCustomAuthenticationFilter {
+import java.io.IOException;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class MyBasicCustomAuthenticationFilter implements Filter {
+	
+	/*
+	 * It will create an Authentication Object based on the request we go
+	 * Then it will pass that object to the AuthenticationManager
+	 */
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		
+		
+	}
+
+	
 }
