cdda3b364c1caadd2a89aa850d7121097f36bfa4
vikashkmr519 <vikashkmr519@gmail.com>
2022-09-11 23:13:27 +0530

    Resource server using database to fetch token

--- ResourceServer/src/main/java/com/oauth2/ResourceServer/config/ResourceServerConfig.java
+++ ResourceServer/src/main/java/com/oauth2/ResourceServer/config/ResourceServerConfig.java
@@ -1,13 +1,49 @@
 package com.oauth2.ResourceServer.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 @SpringBootConfiguration
 @EnableResourceServer
-public class ResourceServerConfig{
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	
 	/*
 	 * for this Resource server, AuthorizationServer3 is the package for Auth server
 	 */
+	
+	@Autowired
+	private DataSource datasource;
+	
+	
+	/*
+	 * This is required if we are using black boarding concept,
+	 * when tokens are stored or fetched from the data base
+	 * 
+	 * 
+	 * AuthrisarionServer would also have stored the token in the database
+	 * and resource server will fetch the token from the database.
+	 * Just provide database url ,username password, and go ahead
+	 * 
+	 * But need to tell the tokenStore that we are going to use database.
+	 */
+	@Autowired
+	public TokenStore tokenStore() {
+		return new JdbcTokenStore(datasource);
+	}
+
+
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.tokenStore(tokenStore());
+	}
+	
+	
+	
 }
--- ResourceServer/src/main/java/com/oauth2/ResourceServer/controller/HelloController.java
+++ ResourceServer/src/main/java/com/oauth2/ResourceServer/controller/HelloController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 	
+	/*
+	 * Resource server is a place which provides some data or access, it should have 
+	 * some api's to hit
+	 */
 	
 	@GetMapping("/hello")
 	public String hello() {
